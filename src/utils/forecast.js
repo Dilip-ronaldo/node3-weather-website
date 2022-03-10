67ba5e84c928a6d4703f80359d89ce6f0a4cce18
unknown <dilipcse15@gmail.com>
2022-03-10 12:53:51 -0800

    Added data to forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,6 @@ const request = require('postman-request')
 const forcast = (latitude, longitude, callback) => {
     
     const forecastUrl = 'http://api.weatherstack.com/current?access_key=f9413f8d01c811e76974b86f9ad386bc&query='+latitude+','+longitude+'&units=m'
-
     request({ url: forecastUrl, json: true }, (error, {body}={}) => { //destructure response to {body} as we need only body from response
             if (error) {
                 callback("Unable to connect to the service !!")
@@ -15,6 +14,7 @@ const forcast = (latitude, longitude, callback) => {
                     const currentData = body.current
                     data = currentData.weather_descriptions[0]
                     data+=". It is currently " + currentData.temperature + " degrees out. It feels like " + currentData.feelslike
+                    data+=" the wind speed is "+ currentData.wind_speed+" and the humidity is "+currentData.humidity
                     callback(undefined, data)
                 }
         
